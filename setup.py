448ecb4d5dedf2f096a60ceeff85b48e1435fab7
fish <fisseha.137@gmail.com>
2022-08-22 13:35:22 +0300

    setup updated

---
+++ setup.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+
+"""The setup script."""
+
+from setuptools import setup, find_packages
+
+with open('README.md') as readme_file:
+    readme = readme_file.read()
+
+requirements = ['pandas>=1.1.0', 'numpy>=1.19.0', ]
+
+test_requirements = ['pytest>=3', ]
+
+setup(
+    author="Fisseha Estifanos",
+    email="fisseha.137@gmail.com",
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A repository structure for the Telecoms data set analysis and insight generation project.",
+    install_requires=requirements,
+    long_description=readme,
+    include_package_data=True,
+    keywords='scripts',
+    name='scripts',
+    packages=find_packages(include=['scripts', 'scripts.*']),
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/Fisseha-Estifanos/Tellecomm',
+    version='0.1.0',
+    zip_safe=False,
+)
