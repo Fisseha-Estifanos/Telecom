43afaee9a76ab62456d9a253fc093d395e0b5034
fish <fisseha.137@gmail.com>
2022-08-26 22:32:10 +0300

    refactored mean and median imputer methods

--- scripts/dataCleaner.py
+++ scripts/dataCleaner.py
@@ -10,8 +10,9 @@ class dataCleaner():
     """
     A data cleaner class
     """
-    def __init__(self, df: pd.DataFrame) -> None:
-        self.df = df
+    def __init__(self) -> None:
+    #def __init__(self, df: pd.DataFrame) -> None:
+        #self.df = df
         print('Data cleaner in action.')
 
     def remove_unwanted_cols(self, cols: list) -> pd.DataFrame:
@@ -74,12 +75,12 @@ class dataCleaner():
 
         Returns
         =-----=
-        self.df: pandas dataframe
+        df: pandas dataframe
             The modified dataframe
         """
-        self.df['Start'] = pd.to_datetime(self.df['Start'], errors='coerce')
-        self.df['End'] = pd.to_datetime(self.df['End'], errors='coerce')
-        return self.df
+        df['Start'] = pd.to_datetime(df['Start'], errors='coerce')
+        df['End'] = pd.to_datetime(df['End'], errors='coerce')
+        return df
 
     def fill_na(self, type: str, df: pd.DataFrame, 
                 cols: list) -> pd.DataFrame:
@@ -117,3 +118,47 @@ class dataCleaner():
             return self.df
         else:
             print('type must be either mean, median or mode')
+
+    def fillWithMedian(self, df: pd.DataFrame, cols: list) -> pd.DataFrame:
+        """
+        A function that fills null values with their corresponding median 
+        values
+
+        Parameters
+        =--------=
+        df: pandas data frame
+            The data frame with the null values
+        cols: list
+            The list of columns to be filled with median values
+
+        Returns
+        =-----=
+        df: pandas data frame
+            The data frame with the null values replace with their
+            corresponding median values
+        """
+        print(f'columns to be filled with median values: {cols}')
+        df[cols] = df[cols].fillna(df[cols].median())
+        return df
+
+    def fillWithMean(self, df: pd.DataFrame, cols: list) -> pd.DataFrame:
+        """
+        A function that fills null values with their corresponding mean 
+        values
+
+        Parameters
+        =--------=
+        df: pandas data frame
+            The data frame with the null values
+        cols: list
+            The list of columns to be filled with mean values
+
+        Returns
+        =-----=
+        df: pandas data frame
+            The data frame with the null values replace with their
+            corresponding mean values
+        """
+        print(f'columns to be filled with mean values: {cols}')
+        df[cols] = df[cols].fillna(df[cols].mean())
+        return df
