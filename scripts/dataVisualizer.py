4f39e08e0f5bd1d19acb12c89009dc0826646b6c
fish <fisseha.137@gmail.com>
2022-08-28 02:52:10 +0300

    minor parameter modifictions

--- scripts/dataVisualizer.py
+++ scripts/dataVisualizer.py
@@ -12,7 +12,7 @@ class dataVisualizer():
     def __init__(self) -> None:
         print('Data visualizer in action.')
 
-    def plot_pie(df: pd.DataFrame, column: str, title: str) -> None:
+    def plot_pie(self, df: pd.DataFrame, column: str, title: str, largest:int = 10) -> None:
         """
         A function to plot pie charts
 
@@ -24,17 +24,17 @@ class dataVisualizer():
         None: nothing
             Only plots the plot
         """
-        # fig = plt.figure(figsize=(10, 10))
-        col = df[column].value_counts().nlargest(n=10)
+        fig = plt.figure(figsize=(10, 10))
+        col = df[column].value_counts().nlargest(n=largest)
 
         data = col.values
         labels = col.keys()
 
         last_num = len(data)
 
-        colors = sns.color_palette('pastel')[0:last_num]
+        colors = sns.color_palette('muted')[0:last_num]
 
-        plt.pie(data, labels=labels, colors=colors, autopct='%.0f%%')
+        plt.pie(data, labels=labels, colors=colors, autopct='%.000f%%')
         plt.title(title)
         plt.show()
 
